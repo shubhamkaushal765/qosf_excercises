55d4a14926653cdc7d3c8e8b3a7e55fd8a7c6804
shubhamkaushal <shubhamkaushal765@gmail.com>
2024-04-01 16:59:21 +0530

    Add graph generators.

---
+++ graph_utils/random_graph.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def get_random_graph(num_nodes=8, p_edge=0.4, draw_graph=True, seed=12345):
+    """
+    Generate a random graph using the fast G(n,p) model from NetworkX.
+
+    Parameters:
+    - num_nodes (int): Number of nodes in the graph.
+    - p_edge (float): Probability of an edge between any pair of nodes.
+    - draw_graph (bool): Whether to visualize the graph.
+
+    Returns:
+    nx.Graph: The generated random graph.
+    """
+    graph = nx.fast_gnp_random_graph(n=num_nodes, p=p_edge, seed=seed)
+    if draw_graph:
+        nx.draw_kamada_kawai(graph, with_labels=True)
+        plt.show()
+    return graph
